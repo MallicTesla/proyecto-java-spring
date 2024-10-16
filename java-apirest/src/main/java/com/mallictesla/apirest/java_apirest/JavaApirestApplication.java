17b94bfedd7c38acdb6ed8bdc8f55be54dfc9e7b
MallicTesla <el_mallic@hotmail.com>
2024-10-16 17:39:15 -0300

    creando una imagen de docker

--- java-apirest/src/main/java/com/mallictesla/apirest/java_apirest/JavaApirestApplication.java
+++ java-apirest/src/main/java/com/mallictesla/apirest/java_apirest/JavaApirestApplication.java
@@ -3,10 +3,20 @@ package com.mallictesla.apirest.java_apirest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class JavaApirestApplication {
 
 	public static void main(String[] args) {
+
+        // Cargar el archivo .env
+        Dotenv dotenv = Dotenv.configure()
+                .directory("C:/Users/Mallic/Desktop/Programas/proyecto-java-spring/java-apirest")  // Ruta del archivo .env
+                .load();
+
+		// C:\Users\Mallic\Desktop\Programas\proyecto-java-spring\java-apirest\pom.xml
+
 		SpringApplication.run(JavaApirestApplication.class, args);
 	}
 
